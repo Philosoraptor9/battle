0c85a11d13521bdd000acda33dc1f6eeb8a3d273
Philip Hoover <philhoov90@gmail.com>
2019-01-27 15:57:21 -0700

    testing attack functions for enemy and player

--- main.py
+++ main.py
@@ -6,6 +6,26 @@ magic = [{"name": "Fire", "cost": 10, "damage": 60},
 
 
 player = Person(460, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
 
-print(player.generate_spell_damage(0))
-print(player.generate_spell_damage(1))
+running = True
+i = 0
+
+print(bcolors.FAIL + bcolors.BOLD + "An enemy attacks!" + bcolors.ENDC)
+
+while running:
+    print("==================")
+    player.choose_action()
+    choice = input("Choose action: ")
+    index = int(choice) - 1
+
+    if index == 0:
+        damage = player.generate_damage()
+        enemy.take_damage(damage)
+        print("You attacked for ", damage, " damage. Enemy HP:", enemy.get_hp() )
+
+    enemy_choice = 1
+
+    enemy_damage = enemy.generate_damage()
+    player.take_damage(enemy_damage)
+    print("Enemy attacks for ", enemy_damage, " damange. Player HP:", player.get_hp())
